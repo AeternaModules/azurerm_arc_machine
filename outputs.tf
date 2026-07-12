e14b7472fd3a88e3bdabdb7062c26dce7bbfbd44
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:57 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "arc_machines" {
-  description = "All arc_machine resources"
-  value       = azurerm_arc_machine.arc_machines
-}
 output "arc_machines_identity" {
-  description = "List of identity values across all arc_machines"
-  value       = [for k, v in azurerm_arc_machine.arc_machines : v.identity]
+  description = "Map of identity values across all arc_machines, keyed the same as var.arc_machines"
+  value       = { for k, v in azurerm_arc_machine.arc_machines : k => v.identity }
 }
 output "arc_machines_kind" {
-  description = "List of kind values across all arc_machines"
-  value       = [for k, v in azurerm_arc_machine.arc_machines : v.kind]
+  description = "Map of kind values across all arc_machines, keyed the same as var.arc_machines"
+  value       = { for k, v in azurerm_arc_machine.arc_machines : k => v.kind }
 }
 output "arc_machines_location" {
-  description = "List of location values across all arc_machines"
-  value       = [for k, v in azurerm_arc_machine.arc_machines : v.location]
+  description = "Map of location values across all arc_machines, keyed the same as var.arc_machines"
+  value       = { for k, v in azurerm_arc_machine.arc_machines : k => v.location }
 }
 output "arc_machines_name" {
-  description = "List of name values across all arc_machines"
-  value       = [for k, v in azurerm_arc_machine.arc_machines : v.name]
+  description = "Map of name values across all arc_machines, keyed the same as var.arc_machines"
+  value       = { for k, v in azurerm_arc_machine.arc_machines : k => v.name }
 }
 output "arc_machines_resource_group_name" {
-  description = "List of resource_group_name values across all arc_machines"
-  value       = [for k, v in azurerm_arc_machine.arc_machines : v.resource_group_name]
+  description = "Map of resource_group_name values across all arc_machines, keyed the same as var.arc_machines"
+  value       = { for k, v in azurerm_arc_machine.arc_machines : k => v.resource_group_name }
 }
 output "arc_machines_tags" {
-  description = "List of tags values across all arc_machines"
-  value       = [for k, v in azurerm_arc_machine.arc_machines : v.tags]
+  description = "Map of tags values across all arc_machines, keyed the same as var.arc_machines"
+  value       = { for k, v in azurerm_arc_machine.arc_machines : k => v.tags }
 }
 
